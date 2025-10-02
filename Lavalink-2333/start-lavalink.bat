de1ba6a61efb07635929a043ac19df3339f387e0
OGGY <desigamers003@gmail.com>
2025-10-02 19:38:27 +0100

    Delete Lavalink-2333/start-lavalink.bat

--- Lavalink-2333/start-lavalink.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: === Lavalink Startup Script ===
-:: Enable native access (fix Unsafe warnings) + run Lavalink
-
-set JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-17   :: <- change if your Java is elsewhere
-set PATH=%JAVA_HOME%\bin;%PATH%
-
-java --enable-native-access=ALL-UNNAMED -jar Lavalink.jar
